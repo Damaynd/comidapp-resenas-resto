0aa71362ee1a3fcb2395a99fb0c21180864146af
Damaynd <fromdamind@gmail.com>
2025-09-30 03:24:03 -0300

    Feat: new Restaurant model, this is a pre-implementation as ER model should be defined first.

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length = 100)
+    address = models.CharField(max_length = 100)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    price = models.IntegerField(choices = [(1,1),(2,2),(3,3),(4,4),(5,5)])
+    url = models.URLField(blank = True, null = True)
+    cuisines = models.ManyToManyField(Cuisine, blank = True)
+    avg_rating = models.FloatField(default = 0)
+    review_count = models.IntegerField(default = 0)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    def __str__(self): return self.name
+
 
 # Create your models here.
