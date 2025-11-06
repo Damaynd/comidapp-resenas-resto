bd99ababbd9ce38f5e670380b5de7ca35f27a0f2
Amanda Chiong <amanda.chiong@ug.uchile.cl>
2025-11-06 16:39:50 -0300

    feat: changed displayed page from index.html to home.html

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -5,10 +5,10 @@ from .models import *
 # Create your views here.
 def index(request):
     lista_restaurantes = Restaurant.objects.all()
-    return render(request, "index.html", {"restaurantes": lista_restaurantes})
+    return render(request, "home.html", {"restaurantes": lista_restaurantes})
 
 
-# Cuando haces click en un restaurante de index.html, esta funcion te llevara a detalle_restaurante.html para ver mas detalles
+# Cuando haces click en un restaurante de home.html, esta funcion te llevara a detalle_restaurante.html para ver mas detalles
 def detalle_restaurante(request, restaurante_id):
     restaurante = Restaurant.objects.get(pk=restaurante_id)
     return render(request, "detalle_restaurante.html", {"restaurante": restaurante})
